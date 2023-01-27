6ac2fce197eee279e12953f1b59ff84a3dd063fb
ShadowSouls98 <ivanacaldero@gmail.com>
2023-01-26 19:31:09 -0600

    playeecontrol

--- Assets/_assets/Ivan/scripts/playerController.cs
+++ Assets/_assets/Ivan/scripts/playerController.cs
@@ -1,9 +1,16 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public Transform player;
+    public WheelCollider frontWColider, backWColider;
+    public Transform frontWtransform, backWtransform;
+    public float _steerAngle=25.0f, _motoForce=1500f, steerAngle;
+    float h, v;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,35 @@ public class playerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        Inputs();
+        Drive();
+        SteerCar();
+        UpdateWheelPos(frontWColider, frontWtransform);
+        //UpdateWheelPos(backWColider, backWtransform);
+    }
+
+    void Inputs() {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
         
+    }
+    void Drive() {
+        backWColider.motorTorque = v * _motoForce;
+    }
+    void SteerCar() {
+        steerAngle= _steerAngle*h;
+        frontWColider.steerAngle= steerAngle;
+    }
+    void UpdateWheelPos(WheelCollider col, Transform t)
+    {
+        Vector3 pos = t.position;
+        Quaternion rot = t.rotation;
+
+        col.GetWorldPose(out pos,out rot);
+        t.position = pos;
+        t.rotation = rot;
     }
 }
