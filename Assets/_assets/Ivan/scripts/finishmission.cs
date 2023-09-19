3a6cd099d3db5c768968d671d853c475838b04a2
ShadowSoul98 <89111519+ShadowSoul98@users.noreply.github.com>
2023-09-18 21:17:10 -0600

    biblioteca en Realise

--- Assets/_assets/Ivan/scripts/finishmission.cs
+++ Assets/_assets/Ivan/scripts/finishmission.cs
@@ -7,22 +7,22 @@ using UnityEngine;
 public class finishmission : MonoBehaviour
 {
     public int bolbook = 0;
-    public Light light3,light1,light2;
+    public List<Light> luz;
     public Transform puerta,puerta1,newpuerta,newpuerta1;
     public Collider portal;
     // Start is called before the first frame update
     void Update()
     {
+        Debug.Log(" "+bolbook);
         if(bolbook == 4){
             Portal();
         }
     }
 
     void Portal(){
-        portal.enabled = true;
-        light1.enabled = true;
-        light2.enabled = true;
-        light3.enabled = true;
+        for(int i=0;i<luz.Count;i++){
+            luz[i].enabled = true;
+        }
         puerta.GetComponent<Transform>().SetPositionAndRotation(newpuerta.position,newpuerta.rotation);
         puerta1.GetComponent<Transform>().SetPositionAndRotation(newpuerta1.position,newpuerta1.rotation);
     }
