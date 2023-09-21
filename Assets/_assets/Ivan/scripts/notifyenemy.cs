665fd8fbddef82f8a9168c83a72e28693aaa2144
ShadowSoul98 <89111519+ShadowSoul98@users.noreply.github.com>
2023-09-20 23:01:19 -0600

    IA persigue al jugador

--- Assets/_assets/Ivan/scripts/notifyenemy.cs
+++ Assets/_assets/Ivan/scripts/notifyenemy.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 public class notifyenemy : MonoBehaviour
 {
-    
+    public stalkerController notity;
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            notity.asignPlayer(other.transform);
+        }
+    }
 }
--- Assets/_assets/Ivan/scripts/stalkerController.cs
+++ Assets/_assets/Ivan/scripts/stalkerController.cs
@@ -6,26 +6,51 @@ public class stalkerController : MonoBehaviour
     public Transform objetive;
     public Transform[] nodes;
     public float offSet = 2;
+    public Transform player;
+    public bool lookPlayer = false;
     private int actualPosition = 0;
+    private float countdown;
 
-    void Start() {
-        if(agent == null){
+    void Start()
+    {
+        if (agent == null)
+        {
             agent = this.gameObject.GetComponent<NavMeshAgent>();
         }
         objetive = nodes[0];
     }
 
-    void Update() {
+    void Update()
+    {
         agent.SetDestination(objetive.position);
-        Vector3 distance;
-        distance = objetive.position - transform.position;
-        if(distance.magnitude<=offSet){
-            actualPosition++;
-            if(actualPosition >= nodes.Length){
-                actualPosition = 0;
+            Debug.Log(objetive.position);
+        if (!lookPlayer)
+        {
+            Vector3 distance;
+            distance = objetive.position - transform.position;
+            if (distance.magnitude <= offSet)
+            {
+                actualPosition++;
+                if (actualPosition >= nodes.Length)
+                {
+                    actualPosition = 0;
+                }
+                objetive = nodes[actualPosition];
             }
-            objetive = nodes[actualPosition];
+        }
+        else{
+            objetive = player;
+            countdown -= Time.deltaTime;
+            if(countdown <= 0f){
+                lookPlayer = false;
+                objetive = nodes[actualPosition];
+            }
         }
     }
-    
+    public void asignPlayer(Transform nPlayer)
+    {
+        player = nPlayer;
+        lookPlayer = true;
+        countdown = 5f;
+    }
 }
